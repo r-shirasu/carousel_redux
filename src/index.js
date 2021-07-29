5a98381f3726e499e36cffd446d43520042c2e89
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-07-29 10:02:22 +0900

    app.jsもnamed exportになおす

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
+import { App } from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/index";
 import reportWebVitals from "./reportWebVitals";
