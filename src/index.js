a119f28c29f0005ce28dc5b2b5bf50d627c10fd2
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-07-29 10:01:17 +0900

    redux関連のファイルはreduxディレクトリに入れる

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/index";
+import { store } from "./redux/index";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
